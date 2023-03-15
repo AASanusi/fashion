c1b68b9191a6f41282241c259f323af947b143b0
AASanusi <alaminm.sanusi@gmail.com>
2023-03-15 19:58:41 +0000

    Import Container and add Container component under Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <h1>Home</h1>
+        <h1>Sign in</h1>
+        <h1>Sign out</h1>
+      </Container>
     </div>
   );
 }
