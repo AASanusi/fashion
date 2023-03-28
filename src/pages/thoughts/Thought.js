37b7e4c05940410dbd20276fee260619018fab91
AASanusi <alaminm.sanusi@gmail.com>
2023-03-28 14:38:41 +0000

    Remove unnecessary React imports

--- src/pages/thoughts/Thought.js
+++ src/pages/thoughts/Thought.js
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Card from "react-bootstrap/Card";
